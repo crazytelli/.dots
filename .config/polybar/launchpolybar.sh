ce961ec1006d5324076d03cd0230e1f17356c39f
Victor <victorcadarso@gmail.com>
2020-08-17 18:47:19 -0300

    Dual monitor setup não funcionou.

--- .config/polybar/launchpolybar.sh
+++ .config/polybar/launchpolybar.sh
@@ -5,8 +5,19 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example >>/tmp/polybar1.log 2>&1 &
+polybar master >>/tmp/polybar1.log 2>&1 & disown
+#polybar second >>/tmp/polybar2.log 2>&1 & disown
 
-echo "Bars launched..."
+#if type "xrandr"; then
+    #for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        #MONITOR=$m polybar --reload master &
+        #MONITOR=$m polybar --reload second &
+    #done
+#else
+    #polybar --reload master &
+#fi
+
